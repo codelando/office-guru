030636fb79b10045e6d15caf260318d2046bea65
codelando <codelando@gmail.com>
2017-08-28 22:53:27 -0300

    Se agrega la tabla de locations que guardara las oficinas

---
+++ database/migrations/2017_08_29_001154_create_locations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->string('address');
+            $table->float('latitude', 10, 6);
+            $table->float('longitude', 10, 6);
+            $table->string('image');
+            $table->string('website');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
