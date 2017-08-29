bb13fec38efb30e7cc3997436778451bcee4a69f
codelando <codelando@gmail.com>
2017-08-28 23:12:37 -0300

    Se agrega el modelo de locations y su correspondiente seeder
    Para correr el seeder ejecutar `php artisan db:seed`, agregara 10
    ubicaciones a la base de datos cada vez que se ejecute.

---
+++ database/factories/LocationFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Location::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->company,
+        'description' => $faker->text(1000),
+        'address' => $faker->address,
+        'latitude' => $faker->randomFloat(6, $min = -58.417593, $max = -58.369609),
+        'longitude' => $faker->randomFloat(6, $min = -34.629177, $max = -34.583809),
+        'image' => 'office-' . $faker->numberBetween(1, 3) . '.jpg',
+        'website' => $faker->domainName,
+    ];
+});
